c38a6b383660078d08a85ba31ad2bd83a1d6b836
CupKido <taler@g.jct.ac.il>
2023-06-14 16:07:04 +0300

    added auto init for client files through server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,30 @@
 const express = require('express');
-
+const { existsSync } = require('fs');
+const { execSync } = require('child_process');
+const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// stages for updating client files:
+// 1. go to client folder, run: npm run build
+// 2. copy 'dist' folder to server folder
+// 3. run: npm start or npm run dev
+
+
+// if dist folder exists in client folder, copy it to server folder
+// if (!existsSync(path.join(__dirname, '../client/dist'))){
+//     console.log('Building client files...');
+//     execSync('cd ' + path.join(__dirname, '../client' + ' && npm run build'));
+// }
+if (!existsSync(path.join(__dirname, 'dist'))) {
+    console.log('Building client files...');
+    execSync('cd ' + path.join(__dirname, '../client' + ' && npm run build'));
+    console.log('Copying dist folder from client to server...');
+    execSync('cp -r ../client/dist .');
+}
 
 app.get('/', (req, res) => {
-
-    res.send('Hello World!');
-
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 
