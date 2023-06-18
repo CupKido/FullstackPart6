a98fa97ecac70df44caa9443322f544beacb0a8a
ron hodadedi <hodadedi@g.jct.ac.il>
2023-06-18 09:31:39 +0300

    add login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import TopNav from "./Components/TopNav/TopNav";
+import Login from "./Pages/Login/Login";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [user, setUser] = useState(localStorage.getItem("User"));
+    const navigate = useNavigate();
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    function handleLogin(username) {
+        setUser(username);
+        localStorage.setItem("User", JSON.stringify(username));
+        navigate("/");
+    }
+
+    function handleLogout() {
+        setUser(null);
+        localStorage.clear();
+        navigate("/Login");
+    }
+
+    return (
+        <>
+            {user && <TopNav onLogout={handleLogout} />}
+            {user ? (
+                <Routes>
+                    <Route path="/" element={<Navigate to="/Info" />} />
+                    <Route path="/Info" element={<Info />} />
+                    <Route path="/Todos" element={<Todos />} />
+                    <Route path="/Posts">
+                        <Route index element={<Posts />} />
+                        <Route path=":id" element={<Post />} />
+                        <Route path="NewPost" element={<NewPost></NewPost>}></Route>
+                    </Route>
+                    <Route path="/Albums">
+                        <Route index element={<Albums />} />
+                        <Route path=":id" element={<Album />} />
+                    </Route>
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            ) : (
+                <Routes>
+                    <Route path="/" element={<Navigate to="/Login" />} />
+                    <Route path="/Login" element={<Login onLogin={handleLogin} />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            )}
+        </>
+    );
 }
 
-export default App
+export default App;
