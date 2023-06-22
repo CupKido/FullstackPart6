ad50a4980e7c948b3ecc1302ebbaf09304b9e9f3
CupKido <taler@g.jct.ac.il>
2023-06-22 09:46:30 +0300

    added api instance

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,9 @@ import TopNav from "./Components/TopNav/TopNav";
 import Login from "./Pages/Login/Login";
 import Info from "./Pages/Info/Info";
 // import Todos from "./Pages/Todos/Todos";
-
+const api = axios.create({
+    baseURL: 'http://verybad.site:80',
+  });
 function App() {
     const [user, setUser] = useState(localStorage.getItem("User"));
     const navigate = useNavigate();
