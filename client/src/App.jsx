b823ec41bcc9084b1d448f3e7e569eca651ca78e
CupKido <taler@g.jct.ac.il>
2023-06-22 09:58:46 +0300

    changed domain

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import Login from "./Pages/Login/Login";
 import Info from "./Pages/Info/Info";
 // import Todos from "./Pages/Todos/Todos";
 const api = axios.create({
-    baseURL: 'http://verybad.site:80',
+    baseURL: 'http://saartaler.site:80',
   });
 function App() {
     const [user, setUser] = useState(localStorage.getItem("User"));
