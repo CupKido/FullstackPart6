409187ef3ecf079e2ec69d089bf9bbc7f381be54
CupKido <taler@g.jct.ac.il>
2023-06-22 18:07:14 +0300

    added posts support

--- client/src/Components/posts/Post.jsx
+++ client/src/Components/posts/Post.jsx
@@ -1,19 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import Comment from './Comment';
 import { Link } from "react-router-dom";
-
-
-function Post({post, index}){
+import ApiContext from "../../ApiContext";
+import { useUser } from "../../UserContext";
+function Post({post}){
     const [extend, setExtend] = useState(false);
     const [showComments, setShowComments] = useState(false);
     const [comments, setComments] = useState([]);
-
+    const [comment, setComment] = useState('');
+    const api = useContext(ApiContext);
+    const user = useUser();
     useEffect(() => {
         if (showComments){
             const getComments = async () => {
-                fetch('https://jsonplaceholder.typicode.com/comments?postId=' + post.id)
-                    .then(response => response.json())
-                    .then(newComments => setComments(newComments))
+                api.get('posts/' + post.userId + '/' + post._id + '/comments')
+                    .then(response => setComments(response.data))
                 }    
             getComments();
         }
@@ -22,9 +23,30 @@ function Post({post, index}){
         }
     }, [showComments])
 
+    const addComment = () => {
+        const commentObj = {
+            name: user?.username ?? 'anonymous',
+            body: comment
+        }
+        api.post('posts/' + post.userId + '/' + post._id + '/CreateComment', commentObj).then((response) => {
+            if (response.status === 201) {
+                alert("Comment added successfuly");
+                setComments([...comments, response.data]);
+                setShowComments(true);
+                setComment('');
+            }
+            else {
+                alert("Comment failed");
+            }
+        }).catch((error) => {
+            console.log(error)
+            alert("Comment failed");
+        });
+    }
+
     return (
         <div style={extend? {borderStyle: "solid", borderColor: "red"}: {}}>
-            {/* TODO: deednt fully understand the porpse of the fiture */}
+            {/* TODO: didnt fully understand the purpse of the feature */}
             <button onClick={() => setExtend((prevVal)=>!prevVal)}>extend</button>
             
             <h2>{post.title}</h2>
@@ -34,6 +56,8 @@ function Post({post, index}){
             </button>
 
             <div style={{maxHeight: 300, overflow: 'scroll'}}>
+                <input type="text" value={comment} onChange={(event) => setComment(event.target.value)} />
+                <button onClick={addComment}>Add Comment</button>
                 {showComments &&
                 comments.map((comment, index) => 
                 <div key={index}>
--- client/src/Components/posts/Posts.jsx
+++ client/src/Components/posts/Posts.jsx
@@ -1,31 +1,56 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useContext } from 'react'
 import { Link, useParams } from 'react-router-dom';
 import Post from './Post';
 import { useUser } from '../../UserContext';
-
+import ApiContext from '../../ApiContext';
 function Posts() {
-  
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
   const user = useUser();
   const [posts, setPosts] = useState([]);
   const {selected} = useParams();
-
+  const api = useContext(ApiContext);
   useEffect(() => {
     async function loadPosts(){
       console.log(user);
-      fetch('https://jsonplaceholder.typicode.com/posts?userId=' + user.id)
-        .then(respond => respond.json())
-        .then(posts => setPosts(posts))
+      api.get('/posts/' + user._id)
+        .then(response => setPosts(response.data))
         .catch((error=>console.log(error)));
     }
 
     loadPosts();
   }, [])
 
+  const AddPost = () => {
+    const post = {
+      title: title,
+      body: body
+    }
+    api.post('/posts/' + user._id + '/CreatePost', post)
+    .then((response) => {
+      if (response.status === 201) {
+        alert("Post added successfuly");
+        setPosts([...posts, response.data]);
+        setTitle('');
+        setBody('');
+      }
+      else {
+        alert("Post failed");
+      }
+    }).catch((error) => {
+      console.log(error)
+      alert("Post failed");
+    });
+  }
+
   return (
     <>
+      <input type="text" value={title} onChange={(event) => setTitle(event.target.value)} />
+      <input type="text" value={body} onChange={(event) => setBody(event.target.value)} />
+      <button onClick={AddPost}>Add Post</button>
       {posts.map((post, index) =>
           <div>
-            <Post id={index} post={post}/>
+            <Post post={post}/>
             <hr style={{height: 5, backgroundColor: 'black'}}/>
           </div>
         )}
